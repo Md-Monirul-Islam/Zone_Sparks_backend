d343bfdc9bbaed2f4c5fd8c2dc694e22c5e76c76
unknown <md.monirulmunna.cse@gmail.com>
2024-10-31 11:54:43 +0600

    add product show for customer

--- home_page_app/urls.py
+++ home_page_app/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.static import static
 from .import views
 
 urlpatterns = [
-    
+    path('products-show-in-hoem-page/', views.ProductListCreateView.as_view(), name='products-show-in-hoem-page'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home_page_app/views.py
+++ home_page_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from store_app.models import Product
+from store_app.serializers import ProductSerializer
 
 # Create your views here.
+class ProductListCreateView(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
